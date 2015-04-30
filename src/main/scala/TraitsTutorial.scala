ae43f361ceb2a3e4bb61705da7becbf2eaef6e3d
Cody Goodman <codygman.consulting@gmail.com>
2015-04-30 15:49:39 -0500

    initial commit

---
+++ src/main/scala/TraitsTutorial.scala
@@ -0,0 +1,11 @@
+ // trait Similarity {
+
+// object Main extends App {
+//   Console.println("Hello World!")
+// }
+
+
+
+object TraitsTutorial {
+  def main(args: Array[String]) = println("Hi!")
+}
